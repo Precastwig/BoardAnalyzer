320f580f7889585594cbeecd5b46e6f8112e5a66
Precastwig <Precastwig@users.noreply.github.com>
2023-05-26 11:15:43 +0100

    Remove Cpp

--- include/States.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef STATES
-#define STATES
-
-
-
-#endif
--- src/InputRegister.cpp
+++
@@ -1,5 +0,0 @@
-#include <InputRegister.hpp>
-
-void InputRegister::register_input(const sf::Event::EventType& event, std::function<void(sf::Event&)> callback) {
-    m_input_map[event].push_back(callback);
-}
